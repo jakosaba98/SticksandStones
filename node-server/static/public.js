4f3d38d6177136e0e0719a99affd4f802a0d0ff5
ENSOO <tomasellolorenzo1@gmail.com>
2019-10-28 15:05:21 +0100

    sistemazioni...

--- node-server/static/public.js
+++ node-server/static/public.js
@@ -24,20 +24,6 @@ const routes = async (fastify, options) => {
     fastify.get('/azienda.html',async(req,res)=>{
         res.sendFile('azienda.html');
     })
-    fastify.get('/orari.html',async(req,res)=>{
-        // return login page if not logged in
-        if(!req.session.user_id)
-            res.redirect('/login.html');
-        else
-            res.sendFile('orari.html');
-    })
-    fastify.get('/mappa.html',async(req,res)=>{
-        // return login page if not logged in
-        if(!req.session.user_id)
-            res.redirect('/login.html');
-        else
-            res.sendFile('mappa.html');
-    })
     fastify.get('/test_mappe.html',async(req,res)=>{//remove plz!!!!!!!!!!
         res.sendFile('test_mappe.html');
     })
