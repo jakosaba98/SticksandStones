c74004c6f7e51011045c70fe1dfefe7503a9372e
ENSOO <tomasellolorenzo1@gmail.com>
2019-11-04 12:54:52 +0100

    First partial implementation

---
+++ node-server/services/websocket.js
@@ -0,0 +1,75 @@
+const Influx = require('influx');
+const config = require('../config.json');
+const refreshRate=4000;//ms
+const influxConfig = {
+    host: config.ipaddress,
+    database: config.influx.database,
+    port: config.influx.port,
+    username: config.influx.username,
+    password: config.influx.password,
+    schema: [
+    {
+        measurement: config.influx.measurement,
+        fields: {
+          count: Influx.FieldType.FLOAT,
+          doors: Influx.FieldType.BOOLEAN,
+          lat: Influx.FieldType.FLOAT,
+          lon: Influx.FieldType.FLOAT
+        },
+        tags: [
+          'id'
+        ]
+    }
+    ]
+}
+
+const routes = async (fastify, options) => {
+    //websocket is listening
+    fastify.get('/', { websocket: true }, (connection, req) => {
+        
+            console.log(["req",req])
+        if(req.session && req.session.name)// solo se loggato!
+        {
+            connection.socket.on('connection',socket=>{
+                socket.on('message',message=>{
+                    console.log(['MESSAGGIO',message]);
+                })
+                socket.on('error',error=>{
+                    console.log(error);
+                    socket.close();
+                })
+            });
+
+            connection.socket.on('message', message => {
+                console.log(['messaggio',message/*,req*/]);
+                connection.socket.id=message;
+                clearInterval(connection.socket.interval);
+                connection.socket.interval=setInterval(()=>{
+                    const influx = new Influx.InfluxDB(influxConfig);
+                    let timestamp=new Date().getTime()*1000000-refreshRate*1000000;
+                    let queryString=`select *
+                                    from ${config.measurement}
+                                    where id = ${Influx.escape.stringLit(connection.socket.id)}`;
+                    if(!isNaN(timestamp))
+                        queryString+=' and "time" > '+timestamp;
+                    queryString+=' order by time desc';
+                    influx.query(queryString)
+                        .then(results => connection.socket.send(results))
+                        .catch((err)=>console.log(err));//any better way to log this??
+                },refreshRate);
+            })/*
+            connection.socket.on('close', (code,reason) => {
+                console.log(['Connessione chiusa',code,reason]);
+                clearInterval(connection.socket.interval);
+            })*/
+            connection.socket.on('error', error => {
+                console.log(error);
+            });
+        }
+        else
+        {
+            connection.socket.close();
+        }
+    });
+}
+module.exports=routes;
